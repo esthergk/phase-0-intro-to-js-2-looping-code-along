35669e440b293955b63afbd6ae3483ed0936546c
Esther <esthergrace808kim@gmail.com>
2023-12-20 14:24:38 -1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const arrayForMessages = []
+    for (let i = 0; i < names.length; i++) {
+        arrayForMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return arrayForMessages
+}
+
+function countDown() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
